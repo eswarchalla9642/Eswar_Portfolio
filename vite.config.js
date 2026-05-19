e5d302b93891e73521dd1d992b37bb48f242168d
eswarchalla9642 <eswarchalla9642@gmail.com>
2026-05-19 08:39:16 +0530

    updated config, index path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Eswar_Portfolio/', // Set the base path for GitHub Pages
-});
-
+  base: '/Eswar_Portfolio/',
+})
